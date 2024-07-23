411c02fcf7a3ab0d79bab2eecce55d797477a5ac
Manish_k <101657695+ManishK-mky@users.noreply.github.com>
2024-07-23 21:51:37 +0530

    Update mapRoute.js

--- routes/mapRoute.js
+++ routes/mapRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/map', function(req, res) {
+router.get('/', function(req, res) {
     res.render("index"); 
 });
 
